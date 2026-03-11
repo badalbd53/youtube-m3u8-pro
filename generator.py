4a969fb80b0d9edccc1c709c6de71aa2817fcc69
badalbd53 <70716875+badalbd53@users.noreply.github.com>
2026-03-11 10:19:35 +0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,23 +1,29 @@
 import json
 import subprocess
 
-with open("channels.json") as f:
-    channels = json.load(f)
-
 playlist = "#EXTM3U\n"
+
+try:
+    with open("channels.json") as f:
+        channels = json.load(f)
+except:
+    channels = []
 
 for ch in channels:
     try:
-        stream = subprocess.check_output(
-            ["yt-dlp","-f","best","-g",ch["url"]],
+        result = subprocess.check_output(
+            ["yt-dlp", "-f", "best", "-g", ch["url"]],
             stderr=subprocess.DEVNULL
-        ).decode().splitlines()[0]
+        ).decode().splitlines()
 
-        playlist += f'#EXTINF:-1 tvg-logo="{ch["logo"]}",{ch["name"]}\n'
-        playlist += stream + "\n"
+        if len(result) > 0:
+            stream = result[0]
+
+            playlist += f'#EXTINF:-1 tvg-logo="{ch["logo"]}",{ch["name"]}\n'
+            playlist += stream + "\n"
 
     except Exception as e:
-        print("Error:", ch["name"], e)
+        print("Skip:", ch["name"])
 
-with open("playlist.m3u8","w") as f:
+with open("playlist.m3u8", "w") as f:
     f.write(playlist)
