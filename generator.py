70e309db6c8294a13b1e4dcfa2bc5528e1915e5f
badalbd53 <70716875+badalbd53@users.noreply.github.com>
2026-03-11 10:15:23 +0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -9,15 +9,15 @@ playlist = "#EXTM3U\n"
 for ch in channels:
     try:
         stream = subprocess.check_output(
-            ["yt-dlp","-g",ch["url"]],
+            ["yt-dlp","-f","best","-g",ch["url"]],
             stderr=subprocess.DEVNULL
-        ).decode().strip()
+        ).decode().splitlines()[0]
 
         playlist += f'#EXTINF:-1 tvg-logo="{ch["logo"]}",{ch["name"]}\n'
         playlist += stream + "\n"
 
-    except:
-        print("Offline:", ch["name"])
+    except Exception as e:
+        print("Error:", ch["name"], e)
 
 with open("playlist.m3u8","w") as f:
     f.write(playlist)
